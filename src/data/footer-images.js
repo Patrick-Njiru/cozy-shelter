25971a68ce113332eb701a19b94d897d0f1baba7
Patrick Njiru <patricknjiru93@gmail.com>
2024-03-09 06:08:38 +0300

    Add images for the footer

---
+++ src/data/footer-images.js
@@ -0,0 +1,11 @@
+// Footer
+import ig1 from "../assets/footer/footer1.jpg"
+import ig2 from "../assets/footer/footer2.jpg"
+import ig3 from "../assets/footer/footer3.jpg"
+import ig4 from "../assets/footer/footer4.jpg"
+import ig5 from "../assets/footer/footer5.jpg"
+import ig6 from "../assets/footer/footer6.jpg"
+
+const footerImages = [ig1, ig2, ig3, ig4, ig5, ig6]
+
+export default footerImages
