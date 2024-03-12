2267075603a72e9be7bb9bea827d18d18fefcb65
Patrick Njiru <patricknjiru93@gmail.com>
2024-03-13 00:45:00 +0300

    Give footer images dynamic automatic sizes across different screens.

--- src/pages/shared/footer/InstagramImage.jsx
+++ src/pages/shared/footer/InstagramImage.jsx
@@ -2,20 +2,24 @@ import { useState } from "react"
 import PropTypes from "prop-types"
 
 const InstagramImage = ({ image }) => {
-  const [isHovered, setIsHovered] = useState(false)
-  
+	const [isHovered, setIsHovered] = useState(false)
+
 	return (
-		<a
-			href='https://www.instagram.com'
-			target='_blank'
-			className='w-[30%] m-[1%] transition-all duration-800 rounded-xl h-[110px]'
-			style={{
-				background: `url(${image}) no-repeat`,
-				backgroundSize: `${isHovered ? "115% 115%" : "100% 100%"}`,
-			}}
-			onMouseOver={() => setIsHovered(true)}
-			onMouseLeave={() => setIsHovered(false)}
-		></a>
+		<div className='relative w-[31%] m-[1%]'>
+			<img src={image} alt='ig image' className='opacity-0 w-full' />
+			<a
+				href='https://www.instagram.com'
+				target='_blank'
+				className='transition-all duration-900 ease-linear rounded-xl w-100% h-full absolute top-0 start-0 end-0 bottom-0'
+				style={{
+					background: `url(${image}) no-repeat`,
+					backgroundSize: `${isHovered ? "110% 110%" : "100% 100%"}`,
+					backgroundPosition: "center center",
+				}}
+				onMouseOver={() => setIsHovered(true)}
+				onMouseLeave={() => setIsHovered(false)}
+			></a>
+		</div>
 	)
 }
 
