0022b0f7e45844d67d204a5b66cb7bfd0d5172df
Patrick Njiru <patricknjiru93@gmail.com>
2024-03-12 03:47:11 +0300

    Rename footer images.

--- src/data/footer-data.js
+++ src/data/footer-data.js
@@ -1,10 +1,10 @@
 // Footer
-import ig1 from "../assets/footer/footer1.jpg"
-import ig2 from "../assets/footer/footer2.jpg"
-import ig3 from "../assets/footer/footer3.jpg"
-import ig4 from "../assets/footer/footer4.jpg"
-import ig5 from "../assets/footer/footer5.jpg"
-import ig6 from "../assets/footer/footer6.jpg"
+import ig1 from "../assets/footer/footer-1.jpg"
+import ig2 from "../assets/footer/footer-2.jpg"
+import ig3 from "../assets/footer/footer-3.jpg"
+import ig4 from "../assets/footer/footer-4.jpg"
+import ig5 from "../assets/footer/footer-5.jpg"
+import ig6 from "../assets/footer/footer-6.jpg"
 
 const footerImages = [ig1, ig2, ig3, ig4, ig5, ig6]
 
