5f2ae53fc250599ced60a656094e40cc00188b3d
Patrick Njiru <patricknjiru93@gmail.com>
2024-03-13 22:39:35 +0300

    Optimize footer images.

--- src/data/footer.js
+++ src/data/footer.js
@@ -1,10 +1,10 @@
 // Footer
-import ig1 from "../assets/footer/footer-1.jpg"
-import ig2 from "../assets/footer/footer-2.jpg"
-import ig3 from "../assets/footer/footer-3.jpg"
-import ig4 from "../assets/footer/footer-4.jpg"
-import ig5 from "../assets/footer/footer-5.jpg"
-import ig6 from "../assets/footer/footer-6.jpg"
+import ig1 from "../assets/footer/footer-1.webp"
+import ig2 from "../assets/footer/footer-2.webp"
+import ig3 from "../assets/footer/footer-3.webp"
+import ig4 from "../assets/footer/footer-4.webp"
+import ig5 from "../assets/footer/footer-5.webp"
+import ig6 from "../assets/footer/footer-6.webp"
 
 const footerImages = [ig1, ig2, ig3, ig4, ig5, ig6]
 
